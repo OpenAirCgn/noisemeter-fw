ea3789fdd116aad169b1645cc90241b478b6877c
Matthias Krauss <matthias@presseverykey.com>
2019-08-09 22:35:41 +0200

    Moved to float, dBA and dbC seem to work stable now

--- Src/noisemeter.c
+++ Src/noisemeter.c
@@ -26,18 +26,21 @@ extern TIM_HandleTypeDef htim15;
 uint16_t dmaBuf[2*BUFFER_SAMPLES];
 
 // dBA weighting biquad filter
+arm_biquad_casd_df1_inst_f32 dba_filter;
+float dba_coeffs[5*DBA_CASCADE_SIZE] = DBA_CASCADE_COEFFS;
+float dba_state[4*DBA_CASCADE_SIZE];
+float dba_val = 0.0f;
 
-arm_biquad_casd_df1_inst_q15 dBA_filter;
+// dBC weighting biquad filter
+arm_biquad_casd_df1_inst_f32 dbc_filter;
+float dbc_coeffs[5*DBC_CASCADE_SIZE] = DBC_CASCADE_COEFFS;
+float dbc_state[4*DBC_CASCADE_SIZE];
+float dbc_val = 0.0f;
 
-q15_t dBA_coeffs[6*DBA_CASCADE_SIZE] = DBA_CASCADE_COEFFS;
-
-q15_t dBA_state[4*DBA_CASCADE_SIZE];
-uint32_t dbaSampleAvg = 0;
-q15_t dBA_RMS;
+float gain_db = 0.0f;
 
 // I2C output buffer
-
-static uint8_t i2cData[4] = { 1,2,3,4 };
+static float i2cData[2] = { 0,0 };
 
 // forward function for USB audio (tweaked middleware)
 
@@ -65,7 +68,8 @@ void LED_Toggle_Modulo(void) {
 }
 
 void Noisemeter_Init(void) {
-  arm_biquad_cascade_df1_init_q15(&dBA_filter, DBA_CASCADE_SIZE, dBA_coeffs, dBA_state, DBA_FILTER_SHIFT);
+  arm_biquad_cascade_df1_init_f32(&dba_filter, DBA_CASCADE_SIZE, dba_coeffs, dba_state);
+  arm_biquad_cascade_df1_init_f32(&dbc_filter, DBC_CASCADE_SIZE, dbc_coeffs, dbc_state);
   HAL_OPAMP_Start(&hopamp1);
   HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
   HAL_DAC_Start(&hdac1, DAC_CHANNEL_2);
@@ -76,17 +80,26 @@ void Noisemeter_Init(void) {
 
   Noisemeter_SetAnalogGain(0);
 
+
 #ifdef USE_I2C_SLAVE
+  //allow I2C to preemt audio processing. I2C interrupts are short and we want to avoid I2C timeouts.
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+  HAL_NVIC_SetPriority(I2C1_EV_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(I2C1_ER_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 15, 15);
   HAL_I2C_EnableListen_IT(&hi2c1);
 #endif
 }
 
 #ifdef USE_I2C_SLAVE
 void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t transferDirection, uint16_t AddrMatchCode) {
+  LED_Set(1);
   if (transferDirection == I2C_DIRECTION_RECEIVE) { //direction as seen from master
     //read data: pull to buffer
-    ((q15_t*)i2cData)[0] = dBA_RMS;
-    HAL_I2C_Slave_Seq_Transmit_IT(hi2c, i2cData, 4, 0);
+    i2cData[0] = dba_val;
+    i2cData[1] = dbc_val;
+
+    HAL_I2C_Slave_Seq_Transmit_IT(hi2c, (uint8_t*)i2cData, 8, I2C_LAST_FRAME);
   } else {
     __HAL_I2C_GENERATE_NACK(hi2c);  //TODO: read config
   }
@@ -94,66 +107,52 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t transferDirection, ui
 
 void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c) {
   HAL_I2C_EnableListen_IT(hi2c);
+  LED_Set(0);
 }
 
 void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c) {
   __HAL_I2C_GENERATE_NACK(hi2c);  //no further transmission
+  LED_Set(0);
+}
+
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c) {
+  __HAL_I2C_GENERATE_NACK(hi2c);  //no further transmission
+  LED_Set(0);
 }
 
 #endif
 
 
 void Handle_Samples(uint16_t* base, uint16_t numSamples) {
-  //show high level on LED to show we're alive
-  uint16_t min = 0xffff;
-  uint16_t max = 0x0000;
-  for(int i=0;i<numSamples;i++) {
-    uint16_t val = base[i];
-    if (val < min) min = val;
-    if (val > max) max = val;
+  // convert to float, offset to zero
+  float src[numSamples];
+  for (uint16_t i=0; i<numSamples; i++) {
+    src[i] = (float)(base[i]) - 32768.0f;
   }
-  LED_Set(max-min > 0x1000);
+  //do dba weighting
+  arm_biquad_cascade_df1_f32(&dba_filter, src, src, numSamples);
+  float dba_rms;
+  arm_rms_f32( src, numSamples, &dba_rms);
+  dba_val = 20 * log10(dba_rms) + DBA_VAL_OFFSET - gain_db;
 
-  //offset samples to signed numbers, idle near zero (IN-PLACE!)
-  // uint16_t numWords = numSamples / 2;
-  // uint32_t *wordBuf = (uint32_t*)base;
-  // for (int i=0; i<numWords; i++) {
-  //   wordBuf[i] ^=0x80008000;
-  // }
-  
-  // //do dba weighting
-  // q15_t *src = (q15_t*)base;
-  // q15_t dbaSamples[numSamples];
-  // arm_biquad_cascade_df1_q15(&dBA_filter, src, dbaSamples, numSamples);
-
-  
-  // //biquads may offset zero so compensate mean to zero. Important for RMS
-  // q15_t mean;
-  // arm_mean_q15 (dba, numSamples, &mean);  
-  // dbaSampleAvg = ((dbaSampleAvg_SMOOTH-1)*dbaSampleAvg)/dbaSampleAvg_SMOOTH + mean;
-  // arm_offset_q15 ( dba, -dbaSampleAvg/dbaSampleAvg_SMOOTH, dba, numSamples);
-  // arm_rms_q15( dba, numSamples, &dBA_RMS);
-
-#define SMOOTH_FACTOR 0x8000
-
-  uint32_t sqErrSum = 0;
-  for (int i=0; i<numSamples; i++) {
-    uint16_t sample = base[i];
-    uint16_t zero = 31718;//dbaSampleAvg / SMOOTH_FACTOR;
-    int32_t err = (sample-zero);
-    sqErrSum += err*err;
-    dbaSampleAvg = dbaSampleAvg - (dbaSampleAvg/SMOOTH_FACTOR) + sample;
+  // convert input data to float again, offset to zero
+  // double work, save memory - we might reduce block size and avoid this if processing gets tight
+  for (uint16_t i=0; i<numSamples; i++) {
+    src[i] = (float)(base[i]) - 32768.0f;
   }
-  float sqErrAvg = (float)sqErrSum / (float)numSamples;
-  float rms = sqrtf(sqErrAvg);
-  float dba = 20 * log10(rms);
-
+  //do dbc weighting
+  arm_biquad_cascade_df1_f32(&dbc_filter, src, src, numSamples);
+  float dbc_rms;
+  arm_rms_f32( src, numSamples, &dbc_rms);
+  dbc_val = 20 * log10(dbc_rms) + DBC_VAL_OFFSET - gain_db;
 
 #ifdef USE_USB_SERIAL
   char strBuf[100];
 //  sprintf(strBuf, "samples %i dba %7.2f avg %li min %i max %i\n",numSamples, dba, dbaSampleAvg/SMOOTH_FACTOR, min, max);
-  sprintf(strBuf, "dba %7.2f min %i max %i\n", dba, min, max);
+  sprintf(strBuf, "dba %7.2f dbc %7.2f I2C state %i mode %i error %li\n", dba_val, dbc_val,
+    HAL_I2C_GetState(&hi2c1),HAL_I2C_GetMode(&hi2c1),HAL_I2C_GetError(&hi2c1));
   CDC_Transmit_FS((uint8_t*)strBuf, strlen(strBuf));
+
 #endif
 
 #ifdef USE_USB_AUDIO
@@ -177,26 +176,31 @@ void Noisemeter_SetAnalogGain(uint8_t gain) {
     case 0:
       LL_OPAMP_SetFunctionalMode(OPAMP1, LL_OPAMP_MODE_FOLLOWER);
       HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 2048);
+      gain_db = 0.0f;
       break;
     case 1:
       LL_OPAMP_SetFunctionalMode(OPAMP1, LL_OPAMP_MODE_PGA);
       LL_OPAMP_SetPGAGain(OPAMP1, LL_OPAMP_PGA_GAIN_2);
       HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 1024);
+      gain_db = 6.0f;
       break;
     case 2:
       LL_OPAMP_SetFunctionalMode(OPAMP1, LL_OPAMP_MODE_PGA);
       LL_OPAMP_SetPGAGain(OPAMP1, LL_OPAMP_PGA_GAIN_4);
       HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 512);
+      gain_db = 12.0f;
       break;
     case 3:
       LL_OPAMP_SetFunctionalMode(OPAMP1, LL_OPAMP_MODE_PGA);
       LL_OPAMP_SetPGAGain(OPAMP1, LL_OPAMP_PGA_GAIN_8);
       HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 256);
+      gain_db = 18.0f;
       break;
     default:
       LL_OPAMP_SetFunctionalMode(OPAMP1, LL_OPAMP_MODE_PGA);
       LL_OPAMP_SetPGAGain(OPAMP1, LL_OPAMP_PGA_GAIN_16);
       HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 128);
+      gain_db = 24.0f;
       break;
   }
 }
--- flash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dfu-util -d 0x0483:0xdf11 -a 0 -s 0x8000000 -D build/noisemeter-usbaudio.bin
-
